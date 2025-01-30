1b4f5c92a8ca80173f3500173ae6ee88bf03f5df
anton <anton@ripp.phys.st.lab.au.dk>
2025-01-30 17:36:44 +0100

    plotting

--- Simulations/ResolutionCurve/Realistic/analyzer.jl
+++ Simulations/ResolutionCurve/Realistic/analyzer.jl
@@ -13,11 +13,31 @@ energies = collect(10:10:250)
 ##############
 medi=data[:,1]
 spread=data[:,2]
+fiddi=data[:,3]
+fiddispread=data[:,4]
+hunni=data[:,5]
+hunnispread=data[:,6]
+two=data[:,7]
+twospread=data[:,8]
+tree=data[:,9]
+treespread=data[:,10]
 
-fig1 = plot(energies, spread)
+
+
+fig1 = plot(energies, spread, c=:black, label="Ideal")
+plot!(energies, energies - medi, c=:black, label="Syst Ideal")
+plot!(energies,fiddispread, c=:blue, label="50ps")
+plot!(energies, energies - fiddi, c=:blue, label="syst 50ps")
+plot!(energies,hunnispread, c=:red, label="100ps")
+plot!(energies, energies - hunni, c=:red, label="Syst 100ps")
+plot!(energies,twospread, c=:green, label="200ps")
+plot!(energies, energies - two, c=:green, label="Syst 200ps")
+plot!(energies,treespread, c=:magenta, label="350ps")
+plot!(energies, energies - tree, c=:magenta, label="Syst 350ps")
 title!("Energy uncertainty")
 xlabel!("Energy of incident neutron (MeV)")
 ylabel!("Uncertainty in energy measurement (MeV)")
 savefig("plots/uncertainty.png")
 #display(fig1)
+
 
