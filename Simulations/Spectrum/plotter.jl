ca178b46c8cd2ff81972435d09f4d59822d8573a
anton <anton@ripp.phys.st.lab.au.dk>
2025-02-18 12:09:37 +0100

    default message number

--- Simulations/Spectrum/plotter.jl
+++ Simulations/Spectrum/plotter.jl
@@ -43,8 +43,8 @@ end
 medi, spread = statisticing(incidents)
 
 
-fig2 = histogram(cincidents, bins = 100, color=:blue, label="Correct events", alpha=0.5)
-histogram!(fincidents, bins = 100, color=:red, label="False events", alpha=0.5)
+fig2 = histogram(cincidents, bins = 0:1:300, color=:blue, label="Correct events", alpha=0.5)
+histogram!(fincidents, bins = 0:1:300, color=:red, label="False events", alpha=0.5)
 title!("Total energy spectrum")
 xlabel!("Energy (MeV)")
 ylabel!("Counts")
