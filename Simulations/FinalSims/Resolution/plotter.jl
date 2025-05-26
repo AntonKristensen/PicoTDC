d288595233bf67133991272b3430c95a74c5130a
anton <anton@ripp.phys.st.lab.au.dk>
2025-05-26 19:40:59 +0200

    default message number 12738

--- Simulations/FinalSims/Resolution/plotter.jl
+++ Simulations/FinalSims/Resolution/plotter.jl
@@ -53,7 +53,7 @@ hunnimedi, hunnispread = statisticing(hunniincidents)
 
 
 
-fig2 = histogram(incidents[incidents .< medi*2], bins = 250, color=:black, label="Ideal", alpha=1, size=(500,300), dpi=1000)
+fig2 = histogram(incidents[incidents .< medi*2], bins = 250, color=:black, label="Ideal", alpha=1, size=(500,300), dpi=1000, legend=false)
 #histogram!(fiddiincidents[fiddiincidents .< medi*2], bins = 250, color=:blue, label="50ps", alpha=0.3)
 #histogram!(hunniincidents[hunniincidents .< medi*2], bins = 250, color=:red, label="100ps", alpha=0.3)
 
