fb46680646815944a46fa8f4a34965d978437131
Anton <antonrindomkristensen123@gmail.com>
2025-05-30 22:22:37 +0200

    default message number 786

--- FastOscilloscopeData/script.jl
+++ FastOscilloscopeData/script.jl
@@ -25,6 +25,7 @@ filenames = glob("*.txt","SiPM test Anton/")
 fig = plot(legend=false,dpi=600,minorgrid = true, minorticks=4,minorgridstyle=:solid)
 risetimes = []
 fwhms = []
+decaytimes = []
 for file in filenames
     data = readdlm(file,skipstart=6)
 
@@ -34,6 +35,11 @@ for file in filenames
     risestop = data[findfirst(x -> x > max *9 / 10, data[:,2]),1]
     time = risestop-risestart
     push!(risetimes, time)
+
+    decaystop = data[findlast(x -> x > max /10, data[:,2]),1]
+    decaystart = data[findlast(x -> x > max *9 / 10, data[:,2]),1]
+    time = decaystop-decaystart
+    push!(decaytimes, time)
 
     fwhmstart = data[findfirst(x -> x > max /2, data[:,2]),1]
     fwhmstop = data[findlast(x -> x > max /2, data[:,2]),1]
@@ -54,8 +60,12 @@ for file in filenames
 
 end
 
-print("Risetime = ", round(mean(risetimes),sigdigits=3),"ns ± ", round(std(risetimes),sigdigits=3) / sqrt(length(risetimes)),"ns \n")
-print("FWHM = ",round(mean(fwhms),sigdigits=3),"ns ± ", round(std(fwhms) / sqrt(length(fwhms)),sigdigits=3),"ns \n")
+println("Risetime = ", round(mean(risetimes),sigdigits=3),"ns ± ", round(std(risetimes),sigdigits=3) / sqrt(length(risetimes)),"ns. σ = ",round(std(risetimes),sigdigits=3) ,"\n")
+println("Decaytime = ", round(mean(decaytimes),sigdigits=3),"ns ± ", round(std(decaytimes),sigdigits=3) / sqrt(length(decaytimes)),"ns. σ = ",round(std(decaytimes),sigdigits=3) ,"\n")
+println("FWHM = ",round(mean(fwhms),sigdigits=3),"ns ± ", round(std(fwhms) / sqrt(length(fwhms)),sigdigits=3),"ns. σ = ",round(std(fwhms),sigdigits=3) ,"\n")
+
+
+
 
 xlabel!("Time (s)")
 ylabel!("Voltage (V)")
@@ -63,4 +73,21 @@ title!("Fast outpul pulses")
 xlims!(-0.00000001,0.00000004)
 display(fig)
 savefig(fig, "pulsesplot.svg")
+
+# Calculate the error in the estimate of pulse energy by ToT method
+function Eerror(thresholdratio, dthreshold, dtot, tau, dtau) # Thresholdratio is E/V, where E is the peak voltage of a pulse and V is the voltage threshold for ToT. dthreshold is dV/V so the relative uncertainty of the threshold
+    energyratio = 1 ./ thresholdratio
+    tot = tau .* log.(energyratio)
+
+    return sqrt.(dthreshold.^2 .+ log.(energyratio).^2 .* ((dtot ./ tot).^2 .+ (dtau ./tau).^2))
+end
+
+
+ratios = collect(0.0:0.001:0.2)
+fig2 = plot(ratios,100* Eerror(ratios, 0.01, 0.1, mean(decaytimes)*1e9, std(decaytimes)*1e9), legend=false, linewidth=3)
+title!("Pulse Energy uncertainty")
+xlabel!("Threshold / pulse max")
+ylabel!("Energy uncertainty (%)")
+savefig("EnergyError.svg")
+display(fig2)
 
